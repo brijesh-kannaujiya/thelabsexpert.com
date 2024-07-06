87affbf4d05fb19da178c88c8b374c849b9bcefb
Brijesh <bk091192@gmail.com>
2024-07-06 23:37:50 +0530

    update

--- resources/views/admin/bookings/_id.blade.php
+++ resources/views/admin/bookings/_id.blade.php
@@ -1,2 +1,4 @@
-<a href="{{url('admin/booking/')}}{{$booking->id}}" bid="{{$booking->id}}" title="Edit Booking" class="btn btn-outline-secondary btn-sm copyBooking">{{$booking->id}}</a>
-<label>05:30 am to 06:30 am</label>
+<div style="width: 150px">
+    <a href="{{url('admin/booking/')}}{{$booking->id}}" bid="{{$booking->id}}" title="Edit Booking" class="btn btn-outline-secondary btn-sm copyBooking">{{$booking->id}}</a>
+    <label>05:30 am to 06:30 am</label>
+</div>
--- resources/views/admin/bookings/_status_data.blade.php
+++ resources/views/admin/bookings/_status_data.blade.php
@@ -1,70 +1,74 @@
-<td data-select2-id="44">
-    <span id="bookingStatus-716294">
+<td>
+    <div style="width: 300px">
 
-        {{-- @dd(0) --}}
-        <span class="status" style="text-transform: capitalize;">
-            @if ($booking->status->isNotEmpty())
-            {{$booking->status[0]->name}}
-            @endif
+        <span id="bookingStatus-716294">
+
+            {{-- @dd(0) --}}
+            <span class="status" style="text-transform: capitalize;">
+                @if ($booking->status->isNotEmpty())
+                {{$booking->status[0]->name}}
+                @endif
+            </span>
+            <div class="btn-group dropright">
+                <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="View History" class="btn btn-sm btn-outline-dark dropdown-toggle">
+                </button>
+                <div class="dropdown-menu px-2 py-2" style="width: 20rem;">
+                    <ul class="list-group list-group-flush">
+                        @foreach ($booking->status as $status)
+                        <li class="list-group-item">{{$status->name}}: <span class="st_time_716294_on_the_way"> </span>{{$status->pivot->created_at}}</li>
+                        @endforeach
+                    </ul>
+
+                </div>
+            </div>
+            {{-- @json($booking->status) --}}
+            <hr><span class="st_time"> @if ($booking->status->isNotEmpty())
+                {{$booking->status[0]->pivot->created_at}}
+                @endif</span>
+            <hr>
         </span>
-        <div class="btn-group dropright">
-            <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="View History" class="btn btn-sm btn-outline-dark dropdown-toggle">
-            </button>
-            <div class="dropdown-menu px-2 py-2" style="width: 20rem;">
-                <ul class="list-group list-group-flush">
-                    @foreach ($booking->status as $status)
-                    <li class="list-group-item">{{$status->name}}: <span class="st_time_716294_on_the_way"> </span>{{$status->pivot->created_at}}</li>
-                    @endforeach
-                </ul>
 
+
+        <div class="form-group">
+            <label>Set Status</label>
+            <select class="form-control select2" required="required" name="status" id="status">
+                @foreach ($statuses as $status)
+                <option value="{{$status->id}}" @if ($booking->status[0]->id == $status->id) selected
+
+                    @endif >{{$status->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <b>Address: </b> {{$booking->patient->address}}
+        <br>
+        <br>
+
+
+        @if ($booking->tests->isNotEmpty())
+        @foreach ($booking->tests as $test)
+        {{$test->test_name}}
+        <br>
+        @endforeach
+
+        @endif
+
+        <br>
+        <div class="btn-group dropleft mr-2 mb-2 "><button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="View Vials" class="btn btn-sm btn-outline-dark dropdown-toggle">
+                View Vials </button>
+            <div class="dropdown-menu px-2 py-2" style="width: 15rem;">
+                <ul class="mb-0">
+                    @php
+                    $uniqueTests = $booking->tests->unique('vial_id');
+                    @endphp
+
+                    @foreach ($uniqueTests as $test)
+                    <li>{{$test->vial->name}}</li>
+                    @endforeach
+
+                </ul>
             </div>
         </div>
-        {{-- @json($booking->status) --}}
-        <hr><span class="st_time"> @if ($booking->status->isNotEmpty())
-            {{$booking->status[0]->pivot->created_at}}
-            @endif</span>
-        <hr>
-    </span>
 
-
-    <div class="form-group">
-        <label>Set Status</label>
-        <select class="form-control select2" required="required" name="status" id="status">
-            @foreach ($statuses as $status)
-            <option value="{{$status->id}}" @if ($booking->status[0]->id == $status->id) selected
-
-                @endif >{{$status->name}}</option>
-            @endforeach
-        </select>
-    </div>
-
-    <b>Address: </b> {{$booking->patient->address}}
-    <br>
-    <br>
-
-
-    @if ($booking->tests->isNotEmpty())
-    @foreach ($booking->tests as $test)
-    {{$test->test_name}}
-    <br>
-    @endforeach
-
-    @endif
-
-    <br>
-    <div class="btn-group dropleft mr-2 mb-2 "><button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="View Vials" class="btn btn-sm btn-outline-dark dropdown-toggle">
-            View Vials </button>
-        <div class="dropdown-menu px-2 py-2" style="width: 15rem;">
-            <ul class="mb-0">
-                @php
-                $uniqueTests = $booking->tests->unique('vial_id');
-                @endphp
-
-                @foreach ($uniqueTests as $test)
-                <li>{{$test->vial->name}}</li>
-                @endforeach
-
-            </ul>
-        </div>
     </div>
 </td>
