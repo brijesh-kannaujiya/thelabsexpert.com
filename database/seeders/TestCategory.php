952075ff677cdc48bbb657bc5ff2990e646484e0
Brijesh <bk091192@gmail.com>
2024-07-17 22:55:25 +0530

    update dbseed

--- database/seeders/TestCategory.php
+++ database/seeders/TestCategory.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Test;
+use App\Models\TestCategory as ModelsTestCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,7 +18,7 @@ class TestCategory extends Seeder
 
         foreach ($Tests as $test) {
             if ($test->category_id != null) {
-                TestCategory::create([
+                ModelsTestCategory::create([
                     'category_id' => $test->category_id,
                     'test_id' => $test->id
                 ]);
